382663c43b0399cfeacbd6c575a7acf0e6e3df74
Kouhei Maeda <mkouhei@palmtb.net>
2015-06-09 17:18:59 +0900

    Fixes Celery configuration hard code, separated to .ini file.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- pgraph/tasks.py
+++ pgraph/tasks.py
@@ -1,10 +1,36 @@
 # -*- coding: utf-8 -*-
 """pgraph.tasks module."""
+import os
+import sys
 from celery import Celery
 from py_deps import Package
+if sys.version_info < (3, 0):
+    # pylint: disable=import-error
+    import ConfigParser as configparser
+else:
+    import configparser
 
 
-APP = Celery('tasks', backend='amqp', broker='amqp://guest@localhost//')
+def celery_config():
+    """Load Celery configuration."""
+    if os.environ.get('CELERY_CONFIG') is None:
+        sys.stderr.write("Missing env variable CELERY_CONFIG")
+        sys.exit(2)
+
+    config_file = os.path.abspath(os.environ['CELERY_CONFIG'])
+    if not os.path.isfile(config_file):
+        sys.stderr.write("Can't read file: %s\n\n" % config_file)
+        sys.exit(2)
+
+    config = configparser.SafeConfigParser()
+    config.read(config_file)
+    return config
+
+
+CONF = celery_config()
+APP = Celery('tasks',
+             backend=CONF.get('celery', 'backend'),
+             broker=CONF.get('celery', 'broker'))
 
 
 @APP.task
