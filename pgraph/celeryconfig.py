04883c019e447911a5abae258b96b14d4a960a06
Kouhei Maeda <mkouhei@palmtb.net>
2016-03-17 13:44:56 +0900

    Adds noqa.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- pgraph/celeryconfig.py
+++ pgraph/celeryconfig.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 """pgraph celery configuration."""
 # pylint: disable=unused-wildcard-import,wildcard-import
-from pgraph.backend_config import *
+from pgraph.backend_config import *  # noqa
