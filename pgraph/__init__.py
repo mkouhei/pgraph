6edf0b1549750465d22c3a9ac6bc01da5a056ed6
Kouhei Maeda <mkouhei@palmtb.net>
2020-05-27 19:05:45 +0900

    Bumps patch version.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- pgraph/__init__.py
+++ pgraph/__init__.py
@@ -4,6 +4,6 @@
 __project__ = 'pgraph'
 __author__ = 'Kouhei Maeda'
 __email__ = 'mkouhei@palmtb.net'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __repo__ = 'https://github.com/mkouhei/pgraph'
 READTHEDOCS = 'http://pgraph.readthedocs.org'
